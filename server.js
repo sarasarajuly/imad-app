42c9cb3aa392fe4a730d4ce8e3e631dfd874a873
sarasarajuly <31056711+sarasarajuly@users.noreply.github.com>
2017-08-25 00:54:33 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,8 +64,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-two', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send(createTemplate(articleOne));
+});
+
+app.get('/article-two', function (req, res) {
+ res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
