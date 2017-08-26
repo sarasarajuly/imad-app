0e91befdfc75e5671a75bae41481afd876752303
sarasarajuly <31056711+sarasarajuly@users.noreply.github.com>
2017-08-25 20:57:54 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 
+//move the image
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginleft='100px';
+    
+};
