ecab54edc7c823218b3ab7e15d52ee75ea1be6df
sarasarajuly <31056711+sarasarajuly@users.noreply.github.com>
2017-08-25 19:08:39 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/;articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
     //articleName==article-one
     //articles[articleName]={}contentobject for article-one
     var articleName=req.params.articleName;
