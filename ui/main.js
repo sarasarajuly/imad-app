8fab683c2f71f5c1bad66b96827e6e36c9e53a68
sarasarajuly <31056711+sarasarajuly@users.noreply.github.com>
2017-08-26 23:51:28 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,7 @@ button.onclick=function()
         request.send(null);
 };
 //Submit name
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     
@@ -57,6 +56,8 @@ submit.onclick=function(){
         
     
    //Make the request
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
         request.open("Get","http://sarasarajuly.imad.hasura-app.io/submit-name?name=" +name,true);
         request.send(null);
     //make a req to server and send the names
