489ee585310bedd940bdc3df848b008c231c776f
sarasarajuly <31056711+sarasarajuly@users.noreply.github.com>
2017-09-21 20:42:25 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var config={
     user:'sarasarajuly',
     database:'sarasarajuly',
     host:'db.imad.hasura-app.io',
-    port:'5432'
+    port:'5432',
+    password:process.env.DB_PASSWORD
 
 };
 
