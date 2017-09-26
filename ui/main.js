71e8ca5025c819d04f7462ed03620620669dc928
sarasarajuly <31056711+sarasarajuly@users.noreply.github.com>
2017-09-25 17:44:01 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,8 @@
-var button=document.getElementById("counter");
 
-button.onclick=function()
-{
-    //Create a Request object
-    var request=new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            //Take Some action
-            if(request.status===200){
-            var counter=request.responseText;
-            var span=document.getElementById("count");
-            span.innerHTML=counter.toString();
-            
-            }
-            
-        }
-        };
         
     
-   //Make the request
-        request.open("Get","http://sarasarajuly.imad.hasura-app.io/counter",true);
-        request.send(null);
-};
-//Submit name
+  
+//Submit username/password to login
 
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
@@ -38,28 +15,32 @@ submit.onclick=function(){
     request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE){
             //Take Some action
-            if(request.status===200){
-            //var names=['name1','name2','name3','name4'];
-            var names=request.responseText;
-            names=JSON.parse(names);
-            var list='';
-            for(i=0;i<names.length;i++){
-            list += '<li>' + names[i] + '</li>';
+            if(request.status===200)
+            {
+            console.log('user logged in');
+            alert('Logged in successfully');
+          }else if (request.status===403)
+          {
+              alert('username/password is incorrect');
           }
-    
-           var ul=document.getElementById('namelist');
-            ul.innerHTML=list;
+               else if (request.status===500){
+                    alert('Something went wrong on the server');
+                }
+           
             }
             
-        }
+        
         };
         
     
    //Make the request
-    var nameInput=document.getElementById('name');
-    var name=nameInput.value;
-        request.open("Get","http://sarasarajuly.imad.hasura-app.io/submit-name?name=" +name,true);
-        request.send(null);
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+        request.open("POST","http://sarasarajuly.imad.hasura-app.io/login",true);
+        request.setRequestHeader('Content-Type','application/json');
+        request.send(JSON.stringify({usernaname:username,password:password}));
     //make a req to server and send the names
     //capture a list of names and render it as a list
     var names=['name1','name2','name3','name4'];
