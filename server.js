7b122120b342c5d13d6bb8df2b6fdd6fa5096f35
sarasarajuly <31056711+sarasarajuly@users.noreply.github.com>
2017-09-26 22:07:03 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.post('/login',function(req,res){
             res.status(500).send(err.toString());
         }else{
             if(result.rows.length ===0){
-                res.send(403).send('username/password is invalid');
+                res.status(403).send('username/password is invalid');
             }else{
                 //match password
                 var dbString=result.rows[0].password;
@@ -115,7 +115,7 @@ app.post('/login',function(req,res){
                 res.send('User Credentials correct');
             }else
             {
-                res.send(403).send("Username/password is Invalid !!!");
+                res.status(403).send("Username/password is Invalid !!!");
             }
             
         }
